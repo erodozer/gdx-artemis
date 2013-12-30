8277a4ba0de7f46ea2f08d8a2d22368ee5048344
Nicholas Hydock <nhydock@gmail.com>
2013-12-30 02:18:38 -0500

    Improve Animations
    -Allow animations that wrap around a spritesheet
    -Acknowledge the real U and V of a TextureRegion when applying an
    animation.
    --This should fix any problems where an animation is using a texture
    region from an atlas.

--- src/com/badlogic/gdx/artemis/components/Animation.java
+++ src/com/badlogic/gdx/artemis/components/Animation.java
@@ -4,6 +4,7 @@ import com.artemis.Component;
 import com.artemis.ComponentType;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class Animation extends Component {
 	public static ComponentType CType = ComponentType.getTypeFor(Animation.class);
@@ -33,37 +34,57 @@ public class Animation extends Component {
 	 * Current frame index of the animation
 	 */
 	public int index;
+	public Vector2 xy, frames;
+	
 	/**
 	 * Number of frames available in the animation
 	 */
 	public final int frameCount;
 	
-	private final float frameSize;
+	private Vector2 frameSize;
 	
 	/**
 	 * Region coordinates
 	 */
 	public float u1, u2, v1, v2;
+	public float u0, v0;
 	
 	public Animation(Texture spriteSheet, int frameCount, final float rate, final boolean loop)
 	{
-		this(new TextureRegion(spriteSheet), frameCount, rate, loop);
+		this(new TextureRegion(spriteSheet), frameCount, 1, rate, loop);
 	}
 	
 	public Animation(TextureRegion spriteSheet, int frameCount, final float rate, final boolean loop)
 	{
-		this.frameCount = frameCount;
-		this.frameSize = 1.0f/frameCount;
+		this(spriteSheet, frameCount, 1, rate, loop);
+	}
+	
+	public Animation(Texture spriteSheet, int xFrames, int yFrames, final float rate, final boolean loop)
+	{
+		this(new TextureRegion(spriteSheet), xFrames, yFrames, rate, loop);
+	}
+	
+	public Animation(TextureRegion spriteSheet, int xFrames, int yFrames, final float rate, final boolean loop)
+	{
+		this.frameCount = xFrames * yFrames;
 		this.rate = this.next = rate;
 		this.prev = 0;
 		this.loop = loop;
 		this.index = 0;
+		this.frames = new Vector2(xFrames, yFrames);
 		this.done = false;
 		
+		this.u0 = spriteSheet.getU();
+		this.v0 = spriteSheet.getV();
+		this.frameSize = new Vector2((spriteSheet.getU2()-this.u0)/(float)xFrames, 
+	                                 (spriteSheet.getV2()-this.v0)/(float)yFrames);
+		
 		this.u1 = 0;
-		this.u2 = this.frameSize;
+		this.u2 = this.frameSize.x;
 		this.v1 = 0;
-		this.v2 = 1.0f;
+		this.v2 = this.frameSize.y;
+		
+		this.xy = new Vector2();
 	}
 	
 	public void update(float delta)
@@ -86,14 +107,31 @@ public class Animation extends Component {
 		if (index + 1 < this.frameCount)
 		{
 			index++;
-			this.u1 += this.frameSize;
-			this.u2 += this.frameSize;
+			if (xy.x + 1 < frames.x)
+			{
+				xy.x++;
+				this.u1 += this.frameSize.x;
+				this.u2 += this.frameSize.x;
+			}
+			else if (xy.y + 1 < frames.y)
+			{
+				xy.x = 0;
+				xy.y++;
+				this.u1 = 0;
+				this.u2 = this.frameSize.x;
+				this.v1 += this.frameSize.y;
+				this.v2 += this.frameSize.y;
+			}
 		}
 		else if (this.loop)
 		{
 			index = 0;
+			xy.x = 0;
+			xy.y = 0;
 			this.u1 = 0;
-			this.u2 = this.frameSize;
+			this.u2 = this.frameSize.x;
+			this.v1 = 0;
+			this.v2 = this.frameSize.y;
 		}
 		else
 		{
